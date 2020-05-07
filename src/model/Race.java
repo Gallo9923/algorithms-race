88b3d7868f99969a94936cbe5d6c46d9189c03c0
BuildTools <cgp9923@gmail.com>
2020-05-07 12:50:59 -0500

    Ädded ABBThread class

--- src/model/Race.java
+++ src/model/Race.java
@@ -4,82 +4,109 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Race {
-	
-	//Attributes
+
+	// Attributes
 	private BinaryTree binaryTree;
 	private CArrayList arrayList;
 	private LinkedList linkedList;
-	
-	//Constructor
-	public Race() {
+	private long seed;
+
+	// Constructor
+	public Race(long seed) {
 		binaryTree = new BinaryTree();
 		arrayList = new CArrayList();
 		linkedList = new LinkedList();
 
 	}
-	
-	
-	/*
-	public void addIterativeAL(int num) {
-		arrayList.addIterative(num);
-	}
-	
-	
-	public void addIterativeLE(int num) {
-	
-	}
-	
-	public void addIterativeABB(int num) {
-		
-		
-	}
-	
-	
-	public void addRecursiveAL(int num) {
-		
-	}
-	public void addRecursiveLE(int num) {
-		
-	}
-	public void addRecursiveABB(int num) {
-		
-	}	
-	
-	public void deleteIterativeAL() {
-		
-	}
-	public void deleteIterativeLE() {
-		
-	}
-	public void deleteIterativeABB() {
-		
-	}
-	
-	
-	public void deleteRecursiveAL() {
-		
-	}
-	public void deleteRecursiveLE() {
-		
-	}
-	public void deleteRecursiveABB() {
-		
-	}
-	
-	public boolean queryIterativeAL(int num) {
-		return false;
-	}
-	public boolean queryIterativeLE(int num) {
-		return false;
-	}
-	public boolean queryIterativeABB(int num) {
-		return false;
-	}
-	
-	
-	
-	public boolean queryRecursive(int num);
 
-	*/
-	
+	public long addIterativeAL(int n) {
+		arrayList.addIterative(n);
+
+		return System.currentTimeMillis();
+
+	}
+
+	public long addIterativeLE(int n) {
+
+		return System.currentTimeMillis();
+	}
+
+	public long addIterativeABB(int n) {
+
+		return System.currentTimeMillis();
+	}
+
+	public long addRecursiveAL(int n) {
+
+		return System.currentTimeMillis();
+	}
+
+	public long addRecursiveLE(int n) {
+
+		return System.currentTimeMillis();
+	}
+
+	public long addRecursiveABB(int n) {
+
+		return System.currentTimeMillis();
+	}
+
+	public long deleteIterativeAL(int n) {
+
+		return System.currentTimeMillis();
+	}
+
+	public long deleteIterativeLE(int n) {
+
+		return System.currentTimeMillis();
+	}
+
+	public long deleteIterativeABB(int n) {
+
+		return System.currentTimeMillis();
+	}
+
+	public long deleteRecursiveAL(int n) {
+
+		return System.currentTimeMillis();
+	}
+
+	public long deleteRecursiveLE(int n) {
+
+		return System.currentTimeMillis();
+	}
+
+	public long deleteRecursiveABB(int n) {
+
+		return System.currentTimeMillis();
+	}
+
+	public long queryIterativeAL(int num) {
+
+		return System.currentTimeMillis();
+	}
+
+	public long queryIterativeLE(int num) {
+		return System.currentTimeMillis();
+	}
+
+	public long queryIterativeABB(int num) {
+
+		return System.currentTimeMillis();
+	}
+
+	public long queryRecursiveAL(int num) {
+
+		return System.currentTimeMillis();
+	}
+
+	public long queryRecursiveLE(int num) {
+		return System.currentTimeMillis();
+	}
+
+	public long queryRecursiveABB(int num) {
+
+		return System.currentTimeMillis();
+	}
+
 }
--- src/thread/ABBThread.java
+++ src/thread/ABBThread.java
@@ -1,5 +1,60 @@
 package thread;
 
-public class ABBThread extends Thread{
+import javafx.application.Platform;
+import model.Race;
+import ui.RaceControllerGUI;
 
+public class ABBThread extends Thread {
+
+	private Race race;
+	private RaceControllerGUI controller;
+	private String algorithmType;
+	private String algorithmMode;
+	private int n;
+	private long abbT2;
+
+	public ABBThread(Race race, RaceControllerGUI controller, String algorithmType, String algorithmMode, int n) {
+		this.race = race;
+		this.controller = controller;
+		this.algorithmType = algorithmType;
+		this.algorithmMode = algorithmMode;
+		this.n = n;
+	}
+
+	public void run() {
+
+		if (algorithmType.equalsIgnoreCase("ADD")) {
+
+			if (algorithmMode.equalsIgnoreCase("ITERATIVE")) {
+				abbT2 = race.addIterativeABB(n);
+			} else {
+				abbT2 = race.addRecursiveABB(n);
+			}
+
+		} else if (algorithmType.equalsIgnoreCase("SEARCH")) {
+
+			if (algorithmMode.equalsIgnoreCase("ITERATIVE")) {
+				abbT2 = race.queryIterativeABB(n);
+			} else {
+				abbT2 = race.queryRecursiveABB(n);
+			}
+
+		} else {
+
+			if (algorithmMode.equalsIgnoreCase("ITERATIVE")) {
+				abbT2 = race.deleteIterativeABB(n);
+			} else {
+				abbT2 = race.deleteRecursiveABB(n);
+			}
+
+		}
+
+		Platform.runLater(new Thread() {
+			@Override
+			public void run() {
+				controller.updateABBTime(abbT2);
+			}
+		});
+
+	}
 }
